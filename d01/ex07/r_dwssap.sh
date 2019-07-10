5e14bf2f9be5114a9cdd8dedae58559d4f36197e
Joao Zsigmond <jzsigmon@e1z2r10p1.42.us.org>
2019-07-09 22:19:54 -0700

    Ex07 fixed

--- d01/ex07/r_dwssap.sh
+++ d01/ex07/r_dwssap.sh
@@ -1,1 +1,14 @@
-cat /etc/passwd | sed '/^#/ d' | sed -n 'n;p' | cut -d ':' -f 1 | rev | sort -r | tr '\n' ', '
+#!/bin/sh
+
+START=$FT_LINE1
+END=$FT_LINE2
+
+cat /etc/passwd |
+sed '/^#/ d' |
+sed -n 'n;p' |
+cut -d ':' -f 1 |
+rev |
+sort -r |
+head -n $END |
+tail -n $(echo $END - $START | bc) |
+tr '\n' ', '
