5aa0c715fb2fc02d67e8ffa19008af20c2c838bd
Joao Zsigmond <jzsigmon@e1z2r3p31.42.us.org>
2019-07-19 05:53:47 -0700

    D09

---
+++ d09/ft_generic.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_generic.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jzsigmon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/19 05:44:57 by jzsigmon          #+#    #+#             */
+/*   Updated: 2019/07/19 05:50:07 by jzsigmon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_generic(void)
+{
+	char	*str;
+	int		index;
+
+	str = "Tut tut ; Tut tut\n";
+	index = 0;
+
+	while (str[index] != '\0')
+	{
+		write(1, &str[index], 1);
+		index++;
+	}
+}
+
+int		main(void)
+{
+	ft_generic();
+	return (0);
+}
